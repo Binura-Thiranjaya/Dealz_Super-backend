39a1fe46d51be798938d1a8ea9d1f20a52ecb435
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2022-09-26 16:24:48 +0530

    CRUD WITH DB

--- repository/product.repository.js
+++ repository/product.repository.js
@@ -2,57 +2,47 @@ import AppError from '../utils/appError.js';
 import db from '../utils/dbConnect.js';
 
 export const saveProduct = async (data) => {
-    // const { product_id, barcode	, product_name, last_updated_user, last_updated_date, status } = data;
-     try {
-    //     const product = await db.query(
-    //         'INSERT INTO product (product_id, barcode	, product_name, last_updated_user, last_updated_date, status) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-    //         [product_id, barcode	, product_name, last_updated_user, last_updated_date, status]
-    //     );
-        return "Hi";
+     try {      
+        const sqlInsert = "INSERT INTO product (product_id, barcode	, product_name, last_updated_user,status)  VALUES (?,?,?,?,?)";
+        const { product_id, barcode	, product_name, last_updated_user, status } = data;
+        const result = await db.query(sqlInsert, [product_id, barcode	, product_name, last_updated_user, status]);
+        return Promise.resolve(result)
     } catch (err) {
         throw new AppError(err.message, 500);
     }
 }
 
 export const getProducts = async () => {
-    try {
-        return "Hi";
-
-    } catch (err) {
-        throw new AppError(err.message, 500);
-    }
+    const sqlSelect = "SELECT * FROM product";
+    const res=await db.query(sqlSelect);
+    return Promise.resolve(res).catch((err) => {
+        throw new AppError("Internal Failure", 500);
+    });
 }
 
 export const getProductsById = async (id) => {
-    try {
-        return "Hi";
-
-    } catch (err) {
-        throw new AppError(err.message, 500);
-    }
+    const sqlSelect = "SELECT * FROM product WHERE id = ?";
+    const res = await db.query(sqlSelect, [id])
+    return Promise.resolve(res[0]).catch((err) => {
+        throw new AppError("Internal Failure", 500);
+    });
 }
 
 export const updateProduct = async (data) => {
-    const { product_id, barcode	, product_name, last_updated_user, last_updated_date, status } = data;
     try {
-        const product = await db.query(
-            'UPDATE product SET product_id = $1, barcode	 = $2, product_name = $3, last_updated_user = $4, last_updated_date = $5, status = $6 WHERE product_id = $7 RETURNING *',
-            [product_id, barcode	, product_name, last_updated_user, last_updated_date, status, id]
-        );
-        return "Hi";
-    }catch (err) {
+        const sqlUpdate = "UPDATE product SET barcode = ?, product_name = ?, last_updated_user = ?, status = ? WHERE product_id = ?";
+        const { product_id, barcode	, product_name, last_updated_user, status } = data;
+        const result = await db.query(sqlUpdate, [barcode	, product_name, last_updated_user, status, product_id]);
+        return Promise.resolve(result);
+    } catch (err) {
         throw new AppError(err.message, 500);
     }
 }
 
 export const deleteProduct = async (id) => {
-    try {
-        const product = await db.query(
-            'DELETE FROM product WHERE product_id = $1 RETURNING *',
-            [id]
-        );
-        return "Hi";
-    } catch (err) {
-        throw new AppError(err.message, 500);
-    }
+    const sqlDelete = "DELETE FROM product WHERE id = ?";
+    const res = await db.query(sqlDelete, [id])
+    return Promise.resolve(res).catch((err) => {
+        throw new AppError("Internal Failure", 500);
+    });
 }
